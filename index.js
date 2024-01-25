a13cd60eedc67136752a59d5d4f441e2ec7d6cde
João Gabriel Biazon <jbiazonferreira@gmail.com>
2024-01-25 12:44:20 -0300

    desafio tkg docker

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-
+app.get("/", (req, res)=>{
+    res.json("Esta Funcionando!")
+});
 
 app.listen(3000, function() {
     console.log('Servidor funcionando!')
-})
+});
